e8f846cdefeda131b2c2c119311177c65fe0a319
xinglinHe <113151562>
2019-11-20 22:25:08 +0800

    修改启动脚本

--- HomeMonitor/Monitor_run.sh
+++ HomeMonitor/Monitor_run.sh
@@ -1,3 +1,5 @@
-export PYTHONPATH=$(cd 'dirname $0';pwd)
-python3 $(cd 'dirname $0';pwd)/CameraManage.py
+#!/usr/bin/env bash
+
+export PYTHONPATH=$(cd `dirname $0`; pwd)
+python3 $(cd `dirname $0`; pwd)/mCameraManage.py
 
